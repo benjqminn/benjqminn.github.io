988463a39102ca82da71a9df9255ebe4865fe787
benjqminn <benjqmin13@gmail.com>
2025-03-31 15:50:29 -0400

    Client project outline / initial commits

---
+++ itis3135/client_project/scripts/components.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    includeHTML();
+});
+
+function includeHTML() {
+    document.querySelectorAll("[data-include]").forEach(el => {
+        let file = el.getAttribute("data-include");
+        fetch(file)
+            .then(response => response.text())
+            .then(data => el.innerHTML = data)
+            .catch(err => console.error("Error loading file: ", file, err));
+    });
+}

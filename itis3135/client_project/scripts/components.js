0d941086dca5f4590a387457ebae0a6bcdb7f754
benjqminn <benjqmin13@gmail.com>
2025-04-03 12:39:37 -0400

    update

--- itis3135/client_project/scripts/components.js
+++ itis3135/client_project/scripts/components.js
@@ -1,13 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-    includeHTML();
-});
-
 function includeHTML() {
-    document.querySelectorAll("[data-include]").forEach(el => {
+    document.querySelectorAll("[data-include]").forEach((el) => {
         let file = el.getAttribute("data-include");
         fetch(file)
-            .then(response => response.text())
-            .then(data => el.innerHTML = data)
-            .catch(err => console.error("Error loading file: ", file, err));
+            .then((response) => response.text())
+            .then((data) => el.innerHTML = data)
+            .catch((err) => console.error("Error loading file: ", file, err));
     });
 }
+
+document.addEventListener(("DOMContentLoaded"), function () {
+    includeHTML();
+});
